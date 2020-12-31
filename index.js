22928ca06c980a925fe08fc7230c872556621ba0
mjaber <abujaber44@gmail.com>
2020-12-30 19:17:30 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
-// Add your code here
+// function submitData() {
+//     let formData = {
+//         name: "Steve",
+//         email: "steve@steve.com"
+//     };
+
+//     let configObj = {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json",
+//             "Accept": "application/json"
+//         },
+//         body: JSON.stringify(formData)
+//     };
+
+//     fetch("http://localhost:3000/users", configObj)
+//         .then(function(response) {
+//             return response.json();
+//         })
+//         .then(function(object) {
+//             console.log(object);
+//         })
+//         .catch(function(error) {
+//             alert("Bad things! Ragnarők!");
+//             console.log(error.message);
+//         });
+// }
+
+function submitData(name, email) {
+    let formData = {
+        name: name,
+        email: email
+    };
+
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    };
+
+    return fetch("http://localhost:3000/users", configObj)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(object) {
+            let h2 = document.createElement('h2');
+            h2.innerHTML = object.id;
+            document.body.appendChild(h2);
+            console.log(object);
+        })
+        .catch(function(error) {
+            let h3 = document.createElement('h3');
+            h3.innerHTML = error.message;
+            document.body.appendChild(h3);
+            console.log(error.message);
+        });
+}
