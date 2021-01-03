68daaebd7935055b9c3007d77e67e934e789ed6c
ajkelso <ajkelso@gmail.com>
2021-01-03 13:43:08 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    const userInfo = {
+        name: userName,
+        email: userEmail
+    }
+    
+    const configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(userInfo)
+    }
+    return fetch("http://localhost:3000/users", configObj)
+        .then(function(resp) {
+            return resp.json();
+        })
+        .then(function(object) {
+            const p = document.createElement('p')
+            p.innerHTML = object.id
+            document.body.appendChild(p)
+            console.log(object)
+        })
+        .catch(function(error) {
+            const p = document.createElement('p')
+            p.innerHTML = error.message
+            document.body.appendChild(p)
+        })
+}
